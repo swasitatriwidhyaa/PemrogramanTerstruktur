54154bed1221cf48f684501d8a6d0fb002c340f1
swasitatriwidhyaa <151344802+swasitatriwidhyaa@users.noreply.github.com>
2024-03-20 11:22:35 +0700

    Pertemuan 05

---
+++ class1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+class Mahasiswa{
+   	public:
+   		string nama, npm;
+	};
+	
+	int main(){
+		Mahasiswa Mhs;
+		
+		cin>> Mhs.nama;
+		cin>> Mhs.npm;
+		
+		cout<< Mhs.nama << " " << Mhs.npm << endl;
+	}
