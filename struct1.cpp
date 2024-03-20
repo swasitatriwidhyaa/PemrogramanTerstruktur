669de1c2f734eed3e11c46b0565508a8d45b3245
swasitatriwidhyaa <151344802+swasitatriwidhyaa@users.noreply.github.com>
2024-03-20 11:21:22 +0700

    Pertemuan 05

--- struct1.cpp
+++ struct1.cpp
@@ -1,1 +1,15 @@
+#include<iostream>
+using namespace std;
 
+struct Mahasiswa{
+    string nama, npm;
+};
+
+int main(){
+    Mahasiswa Mhs;
+    
+    cin>> Mhs.nama;
+    cin>> Mhs.npm;
+    
+    cout<< Mhs.nama << " " << Mhs.npm;
+}
